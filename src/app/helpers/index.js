3c931c00f93096ba4d5b0e5ae7f557819b1d4b50
Ankit Dutta <geekankit318@gmail.com>
2017-05-24 03:09:18 +0530

    validateUsername() now adds - for spaces

--- src/app/helpers/index.js
+++ src/app/helpers/index.js
@@ -1,11 +1,24 @@
 'use strict';
 
 module.exports = {
+    /**
+     * Check whether the given email address is valid or not
+     * @param  {string} 
+     * @returns {boolean}
+     * 
+     */
     validateEmail  : (email) => {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     },
+    
+    /**
+     * Replace special characters from username and remove spaces with '-'
+     * @param  {string} 
+     * @returns {string}
+     * 
+     */
     validateUsername : (username) => {
-        return username.replace(/[^a-z\d\s]+/gi, "").replace(/ /g,"_");
+        return username.replace(/[^a-z\d\s]+/gi, "").replace(/ /g,"-");
     }
 }
